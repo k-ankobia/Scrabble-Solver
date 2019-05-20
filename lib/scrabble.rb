c5b5d21e56a1cfa2638773e03e33390705dd311a
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-20 18:22:23 +0100

    passed first test for when no input is entered

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,11 @@
 class Scrabble
 
-  def score
+
+  def initialize(word)
+    @word = word
   end
-  
+
+  def score
+    return 0 if @word == nil || " \t\n"
+  end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,5 +1,12 @@
 require 'scrabble'
 
 describe Scrabble do 
-  it { is_expected.to respond_to(:score) }
+
+  context ' When user incorrect input' do 
+    it 'returns 0 when no word is entered' do
+      scrabble = Scrabble.new('')
+      expect(scrabble.score).to eq 0 
+    end
+
+  end
 end
